fd238d2f2fa3f500f6a7393331f7eceac1bf2372
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-05-18 20:12:58 -0700

    update validateIp

--- w1d1-jc/01-validateIp.js
+++ w1d1-jc/01-validateIp.js
@@ -24,12 +24,28 @@ const validateIPv4 = ip => {
 
   for (let el of ipArr) {
     const int = parseInt(el, 10);
-    if (int.toString().length !== el.length)
+    const hasNonIntegers = int.toString().length !== el.length;
+    const greaterThan255 = int > 255;
+    const hasLeadingZero = (el[0] = 0);
+    if (hasNonIntegers || greaterThan255 || hasLeadingZero) {
+      return 'Neither';
+    }
   }
   return 'IPv4';
 };
 
 const validateIPv6 = ip => {
+  const ipArr = ip.split(':');
+
+  for (let el of ipArr) {
+    const hasGreaterThanFourDigits = el.length > 4;
+    const hasEmptyEl = el === '';
+
+    if (hasGreaterThanFourDigits || hasEmptyEl) {
+      return 'Neither';
+    }
+  }
+
   return 'IPv6';
 };
 
@@ -44,9 +60,14 @@ const validateIp = ip => {
 };
 
 // Test cases
+console.log(validateIp('172.16.254.1')); // "IPv4"
+console.log(validateIp('172.1e.254.1')); // "Neither"
+console.log(validateIp('256.16.254.1')); // "Neither"
+console.log(validateIp('172.016.254.1')); // "Neither"
+console.log(validateIp('2001:0db8:85a3:0:0:8A2E:0370:7334')); // "IPv6"
+console.log(validateIp('20012:0db8:85a3:0:0:8A2E:0370:7334')); // "Neither"
+console.log(validateIp('20012:0db8:85a3:0:0:8A2E::7334')); // "Neither"
 console.log(validateIp('172.16')); // "Neither"
 console.log(validateIp('172:16')); // "Neither"
-console.log(validateIp('172.16.254.1')); // "IPv4"
-console.log(validateIp('2001:0db8:85a3:0:0:8A2E:0370:7334')); // "IPv6"
 console.log(validateIp('256.256.256.256')); // "Neither"
 console.log(validateIp('172.16.254.01')); // "Neither" (leading 0)
