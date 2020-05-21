ae3d21c1a0111c0c25cb53513d03e38b4a51c3d5
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-05-20 18:42:15 -0700

    update peakElements

--- w1d3/jc/03-peakElements.js
+++ w1d3/jc/03-peakElements.js
@@ -3,26 +3,41 @@
  *   - nums[i] !== nums[i+1] (no contiguous numbers)
  * Output: Index of any "peak" (integer greater than both neighbours)
  *
- * [1,2,3,1]
- * [3, 1]
- * [3] returns idx 2
- *
- * [1,2,1,3,5,6,4]
- * [1,2,1,3,5,6,4] mp=idx3
- *
+ * Discussion
+ * - Since arr[i] !== arr[i+1], think of the array as slopes, rising and falling, always having to go up or down
  *
  * Algo
  * - O(N)
- *   - Iterate, if nums[i-1] and nums[i+1] < nums[i] return i
+ *   - Iterate, if nums[i+1] < nums[i] return i
+ *      -dont need to check i-1 because we start from beginning and arr[i] can't == arr[i+1]
  *
  * - O(log N)
- *   - find midpoint, check if prev index & next index are <
- *     - if so, return index
- *     - if nums[i-1] < nums[i], recursively call on nums[i...]
+ *   - if the midpoint is in a descending sequence (arr[mid] < arr[mid-1]), peak must lie to the left
+ *   - if the midpoint is in an ascending sequence (arr[mid] > arr[mid-1]), peak must lie to the left
+ *   - there are no other conditions since arr[i] can't === arr[i-1]
+ *
+ *    [1, 2, 1, 3, 5, 6, 4]
+ *    [1, 2, 1] 3 [5, 6, 4] arr[mid] > arr[mid-1]
  */
 
-const findPeakElements = nums => {
-  //
+const findPeakElements = (nums) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    // If descending
+    if (nums[mid] > nums[mid + 1]) {
+      // Check left
+      right = mid;
+      // If ascending
+    } else {
+      // Check right
+      left = mid + 1;
+    }
+  }
+  return left;
 };
 
 console.log(findPeakElements([1, 2, 3, 1])); // 2 (number=3)
